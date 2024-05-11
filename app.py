49b12b424d6f4c159e266585c7fd5728a7f96621
JAFarrow <f.justin@tutanota.com>
2024-05-11 23:27:13 +0200

    visual additions and changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, flash, render_template, request
+from flask import Flask, session, flash, render_template, request, redirect
 from flask_session import Session
 
 app = Flask(__name__)
@@ -7,20 +7,24 @@ app.config["SESSION_TYPE"] = "filesystem"
 app.secret_key = "12345"
 Session(app)
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def serveIndex():
-    return render_template("cardCreation.html")
+    return render_template("cardCreation.html", nav="deck")
 
 @app.route('/deck', methods=["GET", "POST"])
 def serveDeck():
     if request.method == "POST":
         addCardToStorage(request.form.get("question"), request.form.get("answer"))
         flash("New Card Added to Deck")
-        storedDeck = session["deck"]
-        return render_template("deck.html", deck=storedDeck)
+        return redirect("/redirect")
     else:
         storedDeck = session["deck"]
-        return render_template("deck.html", deck=storedDeck)
+        return render_template("deck.html", deck=storedDeck, nav="home")
+    
+@app.route("/redirect", methods=["GET"])
+#This is a hacky way to prevent form resubmission on page reload, not recommended for anything serious :3
+def redirectCheese():
+    return redirect("/deck")
     
 def addCardToStorage(front, back):
     try:
