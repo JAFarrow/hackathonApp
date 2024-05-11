1420e88ff0b7788077f8fd10587ed656d77e5ca1
JAFarrow <f.justin@tutanota.com>
2024-05-11 19:21:13 +0200

    Deck view structure, backend to serve created cards

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask, flash, render_template, request
+from flask import Flask, session, flash, render_template, request
+from flask_session import Session
 
-app = Flask("HackathonApp")
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 app.secret_key = "12345"
+Session(app)
 
 @app.route('/')
 def serveIndex():
-    # flash("Test")
     return render_template("cardCreation.html")
+
+@app.route('/deck', methods=["GET", "POST"])
+def serveDeck():
+    if request.method == "POST":
+        addCardToStorage(request.form.get("question"), request.form.get("answer"))
+        flash("New Card Added to Deck")
+        storedDeck = session["deck"]
+        return render_template("deck.html", deck=storedDeck)
+    else:
+        storedDeck = session["deck"]
+        return render_template("deck.html", deck=storedDeck)
+    
+def addCardToStorage(front, back):
+    try:
+        test = session["deck"]
+    except KeyError:
+        session["deck"] = []
+    session["deck"].append({"front": front, "back": back}) 
---
+++ static/script.js
@@ -0,0 +1,6 @@
+const modal = document.getElementById("modal");
+const showModal = document.querySelector(".modalBtn");
+
+showModal.addEventListener("click", () => {
+    modal.showModal();
+});
